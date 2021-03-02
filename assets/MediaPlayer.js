1fd45b8ea8ea7d2f725dff97283b9eb4112bbce3
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-03-02 11:45:34 -0600

    El primer plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config)
 {
     this.media = config.el
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin =>{
+        plugin.run(this)
+    })    
 }
 
 MediaPlayer.prototype.play = function ()
@@ -15,5 +24,18 @@ MediaPlayer.prototype.play = function ()
     }
     
 }
+
+MediaPlayer.prototype.mute = function (){
+    this.media.muted = true
+}
+
+MediaPlayer.prototype.unmute = function (){
+    this.media.muted = false
+}
+
+MediaPlayer.prototype.unmuteMute = function (){
+    this.media.muted = !this.media.muted
+} 
+
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
-
+import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video")
-const button =  document.querySelector("button")
+const button =  document.querySelector("#playPause")
+const muteUnmute =  document.querySelector("#unmuteMute")
 
-const player = new MediaPlayer({el:video})
+const player = new MediaPlayer({
+    el:video,
+    plugins:[new AutoPlay()]
+})
 button.onclick = () => player.play();
+muteUnmute.onclick = () => player.unmuteMute();
